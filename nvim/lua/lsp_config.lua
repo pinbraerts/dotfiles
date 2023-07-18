ad7998b870e6f12410b981930f0bd095cf76b54e
pinbraerts <pinbraerts@ya.ru>
2023-07-18 21:23:02 +0300

    snip maps

--- nvim/lua/lsp_config.lua
+++ nvim/lua/lsp_config.lua
@@ -83,6 +83,26 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
 local luasnip = require('luasnip')
 local cmp = require('cmp')
+local function snip_next (fallback)
+    if cmp.visible() then
+        cmp.select_next_item()
+    elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+    else
+        fallback()
+    end
+end
+snip_next = cmp.mapping(snip_next)
+local function snip_prev (fallback)
+    if cmp.visible() then
+        cmp.select_prev_item()
+    elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+    else
+        fallback()
+    end
+end
+snip_prev = cmp.mapping(snip_prev)
 cmp.setup {
 	snippet = {
 		expand = function(args)
@@ -93,32 +113,17 @@ cmp.setup {
 		{ name = 'nvim_lsp' },
 		{ name = 'luasnip' },
 	},
-	mapping = cmp.mapping.preset.insert({
-		['<C-p>'] = cmp.mapping.scroll_docs(-4), -- Up
-		['<C-n>'] = cmp.mapping.scroll_docs(4), -- Down
-		-- ['<Tab>'] = cmp.mapping.snip_next(),
-		-- ['<S-Tab>'] = cmp.mapping.snip_prev(),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<CR>'] = cmp.mapping.confirm {
+	mapping = cmp.mapping.preset.insert {
+		['<c-u>'] = cmp.mapping.scroll_docs(-4),
+		['<c-d>'] = cmp.mapping.scroll_docs(4),
+		['<tab>'] = snip_next,
+		['<s-tab>'] = snip_prev,
+		['<c-n>'] = snip_next,
+		['<c-p>'] = snip_prev,
+		['<c-space>'] = cmp.mapping.complete(),
+		['<cr>'] = cmp.mapping.confirm {
 			behavior = cmp.ConfirmBehavior.Replace,
 			select = true,
 		},
-		['<c-n>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			else
-				fallback()
-			end
-		end, { 'i', 's' }),
-		['<c-p>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { 'i', 's' }),	}),
-	}
+    },
+}
