6a3de482d3b6855f54624f32deb8ab5628e8eb3d
Дмитрий Ширшов <shirshov@trueconf.ru>
2023-08-01 20:40:18 +0300

    Style neovide

--- nvim/lua/style.lua
+++ nvim/lua/style.lua
@@ -10,3 +10,8 @@ vim.fn.sign_define('DiagnosticSignError'   , { text = '❗' })
 vim.fn.sign_define('DiagnosticSignWarn'    , { text = '⚠️ ' })
 vim.fn.sign_define('DiagnosticSignInfo'    , { text = 'ℹ️' })
 vim.fn.sign_define('DiagnosticSignHint'    , { text = '💡' })
+
+if vim.fn.has('g:neovide') then
+    vim.g.neovide_cursor_vfx_mode = 'ripple'
+    vim.g.neovide_cursor_animate_command_line = false
+end
