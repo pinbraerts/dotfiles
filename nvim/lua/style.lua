e0826c6bb12379b0df5037273f3076d21c6916b1
Dmitry Shirshov <pinbraerts@ya.ru>
2023-11-14 21:51:28 +0300

    Improve icons

--- nvim/lua/style.lua
+++ nvim/lua/style.lua
@@ -1,12 +1,13 @@
 vim.fn.sign_define('DapBreakpoint'         , { text = '🔴', texhl = 'DapBreakpoint' })
-vim.fn.sign_define('DapBreakpointCondition', { text = '🟤', texhl = 'DapBreakpoint' })
+vim.fn.sign_define('DapBreakpointCondition', { text = '🟠', texhl = 'DapBreakpoint' })
 vim.fn.sign_define('DapBreakpointRejected' , { text = '⭕', texhl = 'DapBreakpoint' })
-vim.fn.sign_define('DapLogPoint'           , { text = '🟣', texhl = 'DapLogPoint'  })
-vim.fn.sign_define('DapStopped'            , { text = '🟡', texhl = 'DapStopped'    })
-vim.fn.sign_define('DiagnosticSignError'   , { text = '❗' })
-vim.fn.sign_define('DiagnosticSignWarn'    , { text = '⚠️ ' })
-vim.fn.sign_define('DiagnosticSignInfo'    , { text = 'ℹ️' })
-vim.fn.sign_define('DiagnosticSignHint'    , { text = '💡' })
+vim.fn.sign_define('DapLogPoint'           , { text = '🟣', texhl = 'DapLogPoint'   })
+vim.fn.sign_define('DapStopped'            , { text = '🔹', texhl = 'DapStopped'    })
+vim.fn.sign_define('DiagnosticSignError'   , { text = '❗', texhl = 'DiagnosticSignError' })
+vim.fn.sign_define('DiagnosticSignWarn'    , { text = '⚠️ ', texhl = 'DiagnosticSignWarn'  })
+vim.fn.sign_define('DiagnosticSignInfo'    , { text = '🔍', texhl = 'DiagnosticSignInfo'  })
+vim.fn.sign_define('DiagnosticSignHint'    , { text = '💡', texhl = 'DiagnosticSignHint'  })
+-- 📌
 
 if vim.g.neovide then
     vim.g.neovide_cursor_vfx_mode = 'ripple'
