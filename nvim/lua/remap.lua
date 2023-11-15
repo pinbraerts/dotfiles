046002b9046a9dc2dbd1de006f23c9e03fd3bcd6
Дмитрий Ширшов <shirshov@trueconf.ru>
2023-11-15 12:53:33 +0300

    fix smart cd

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -12,9 +12,12 @@ vim.keymap.set('n', ']i', function () v.goto_next { severity = vs.INFO } end, { 
 vim.keymap.set('n', 'cd', function ()
     if vim.b.gitsigns_status_dict and vim.b.gitsigns_status_dict.root then
         vim.cmd.cd(vim.b.gitsigns_status_dict.root)
-    elseif #vim.lsp.get_active_clients() then
-        vim.cmd.cd(vim.lsp.get_active_clients()[1].config.root_dir)
+        return
+    end
+    local clients = vim.lsp.get_active_clients()
+    if clients ~= nil and #clients and clients[1].config ~= nil and clients[1].config.root_dir ~= nil then
+        vim.cmd.cd(clients[1].config.root_dir)
     else
-        vim.cmd.cd(vim.fn.dirname(vim.fn.bufname()))
+        vim.cmd.cd(vim.fs.dirname(vim.fn.bufname()))
     end
 end, { desc = '[cd] git root or lsp root or current file directory' })
