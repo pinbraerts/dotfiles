2233a17bbfc21b1ffb3156489260420cd5d24256
Dmitry Shirshov <shirshov.da@phystech.edu>
2024-01-28 20:51:15 +0300

    source bash_completion in local .bashrc

--- bash/setup.sh
+++ bash/setup.sh
@@ -1,5 +1,4 @@
 set -o vi
 
-[ -f /usr/share/bash-completion/bash_completion ] && . /usr/share/bash-completion/bash_completion
 [ -f ~/.config/aliases.sh ] && . ~/.config/aliases.sh
 [ -f ~/.config/bash/prompt.sh ] && . ~/.config/bash/prompt.sh
