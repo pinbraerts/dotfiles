77ecd2e3fde76a69d703e1b3c69a7ae60e496561
Dmitry Shirshov <shirshov.da@phystech.edu>
2024-02-13 23:34:38 +0300

    surf: add scrolling script

---
+++ .surf/script.js
@@ -0,0 +1,93 @@
+function page_percent(percent) {
+	return window.innerHeight * percent / 100;
+}
+
+var up = window.scrollByLines
+	? function () { window.scrollByLines(-5); }
+	: function () { window.scrollBy(0, -page_percent(10)); }
+;
+
+var down = window.scrollByLines
+	? function () { window.scrollByLines(5); }
+	: function () { window.scrollBy(0, page_percent(10)); }
+;
+
+var pageup = window.scrollByPages
+	? function () { window.scrollByPages(-1); }
+	: function () { window.scrollBy(0, page_percent(95)); }
+;
+
+var pagedown = window.scrollByPages
+	? function () { window.scrollByPages(1); }
+	: function () { window.scrollBy(0, -page_percent(95)); }
+;
+
+function right() {
+	window.scrollBy(page_percent(10), 0);
+}
+
+function left() {
+	window.scrollBy(-page_percent(10), 0);
+}
+
+function home() {
+	window.scroll(0, 0);
+}
+
+function bottom() {
+	window.scroll(0, document.body.scrollHeight);
+}
+
+// If you don't like default key bindings, customize here.
+// if you want to use the combination 'Ctrl + b' (for example), use '^b'
+var bindings = {
+	'h': left,
+	'l': right,
+	'k': up,
+	'j': down,
+	'g': home,
+	'G': bottom,
+	'd': pageup,
+	'u': pagedown,
+}
+
+function isEditable(element) {
+	if (element.nodeName.toLowerCase() == "textarea") {
+		return true;
+	}
+
+	// we don't get keypress events for text input, but I don't known
+	// if it's a bug, so let's test that
+	if (element.nodeName.toLowerCase() == "input" && element.type == "text") {
+		return true;
+	}
+
+	// element is editable
+	if (document.designMode == "on" || element.contentEditable == "true") {
+		return true;
+	}
+
+	return false;
+}
+
+function keypress(evt) {
+	var target = evt.target;
+
+	// if we're on a editable element, we probably don't want to catch
+	// keypress, we just want to write the typed character.
+	if (isEditable(target)) {
+		return;
+	}
+
+	var key = String.fromCharCode(evt.charCode);
+	if (evt.ctrlKey) {
+		key = '^' + key;
+	}
+
+	var fun = bindings[key];
+	if (fun) {
+		fun();
+	}
+}
+
+window.addEventListener("keypress", keypress, false);
