fbfb24d9d653440a9e90938dffd3cbb97c31d702
Dmitry Shirshov <shirshov.da@phystech.edu>
2024-03-16 21:39:20 +0300

    rdp: move to different file

--- .config/aliases.sh
+++ .config/aliases.sh
@@ -6,10 +6,6 @@ else
 fi
 alias ll="ls -ll"
 alias la="ls -la"
-rdp() {
-	xfreerdp $@ -grab-keyboard /kbd-remap:0x1d=0x3a,0x3a=0x1d 
-	return $?
-}
 which >/dev/null 2>&1 make && alias m=make
 which >/dev/null 2>&1 rg && alias grep=rg
 [ -n $VISUAL ] && alias v=$VISUAL
