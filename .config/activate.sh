cf7b59e995acef9b16bf543300e23a352bade114
Dmitry Shirshov <shirshov.da@phystech.edu>
2024-07-03 10:31:57 +0300

    activate: silence mamba

--- .config/activate.sh
+++ .config/activate.sh
@@ -1,4 +1,4 @@
-source >/dev/null 2>&1 "venv/bin/activate" || test -d venv && micromamba activate -p "$(pwd)/venv" || true
+source >/dev/null 2>&1 "venv/bin/activate" || test -d venv && micromamba activate -p "$(pwd)/venv" >/dev/null 2>&1 || true
 
 if test -f ".env"; then
 	set -a
