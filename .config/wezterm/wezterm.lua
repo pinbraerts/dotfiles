404a94237f4172f8ab83e1012c5ea1ee080cc8c6
Dmitry Shirshov <shirshov.da@phystech.edu>
2024-08-20 16:57:16 +0300

    wezterm: format

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,4 +1,4 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = {}
 local a = wezterm.action
@@ -8,14 +8,14 @@ if wezterm.config_builder then
 end
 
 local function tm(key, mods)
-	return { key = key, mods = mods, action = a.SendKey { key = key, mods = mods } }
+	return { key = key, mods = mods, action = a.SendKey({ key = key, mods = mods }) }
 end
 
 config.force_reverse_video_cursor = true
 -- config.color_scheme = 'Vaughn'
-config.color_scheme = 'AdventureTime'
-config.default_prog = { 'powershell.exe', '-nologo' }
-config.font = wezterm.font 'JetBrains Mono'
+config.color_scheme = "AdventureTime"
+config.default_prog = { "powershell.exe", "-nologo" }
+config.font = wezterm.font("JetBrains Mono")
 config.font_size = 12
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
@@ -26,11 +26,11 @@ config.window_padding = {
 	bottom = 0,
 }
 config.keys = {
-	{ key = "\\", mods = "WIN", action = a.SplitHorizontal { domain = "CurrentPaneDomain" } },
-	{ key = "-", mods = "WIN", action = a.SplitVertical { domain = "CurrentPaneDomain" } },
-	{ key = "[", mods = "WIN", action = a.ActivatePaneDirection 'Prev' },
-	{ key = "]", mods = "WIN", action = a.ActivatePaneDirection 'Next' },
-	{ key = "Delete", mods = "WIN", action = a.CloseCurrentPane { confirm = true } },
+	{ key = "\\", mods = "WIN", action = a.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "-", mods = "WIN", action = a.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "[", mods = "WIN", action = a.ActivatePaneDirection("Prev") },
+	{ key = "]", mods = "WIN", action = a.ActivatePaneDirection("Next") },
+	{ key = "Delete", mods = "WIN", action = a.CloseCurrentPane({ confirm = true }) },
 	tm("Enter", "SHIFT"),
 	tm("Enter", "ALT"),
 	tm("Enter", "CTRL"),
@@ -50,4 +50,3 @@ config.keys = {
 -- config.window_background_opacity = 0.8
 
 return config
-
