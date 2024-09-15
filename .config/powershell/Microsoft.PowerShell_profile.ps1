c63a3e60f80127efe3ed012c28bb047595199886
Dmitry Shirshov <shirshov.da@phystech.edu>
2024-09-15 13:48:54 +0300

    fix starship and wezterm for windows

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -4,16 +4,36 @@ $env:POWERSHELL_UPDATECHECK = "Off"
 $OutputEncoding = [Console]::InputEncoding = [Console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
 Import-Module PSReadLine
-Set-PSReadLineOption -EditMode vi -ViModeIndicator Cursor
+Invoke-Expression (&starship init powershell)
+
+function OnViModeChange
+{
+  if ($args[0] -eq 'Command')
+  {
+    Write-Host -NoNewLine "`e[2 q"
+  } else
+  {
+    Write-Host -NoNewLine "`e[6 q"
+  }
+}
+
+$PSReadLineOptions = @{
+  EditMode = "vi"
+  HistoryNoDuplicates = $true
+  HistorySearchCursorMovesToEnd = $true
+  ViModeIndicator = "Script"
+  ViModeChangeHandler = $Function:OnViModeChange
+}
+Set-PSReadLineOption @PSReadLineOptions
 Set-PSReadLineKeyHandler -Chord Ctrl+w -Function BackwardDeleteWord
 Set-PSReadLineKeyHandler -Chord Ctrl+k -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Chord Ctrl+j -Function AcceptLine
 Set-PSReadLineKeyHandler -Chord Ctrl+y -Function AcceptSuggestion
 
 Set-Item -Path Env:RUSTC_WRAPPER -Value sccache
+Set-Item -Path Env:LC_ALL -Value "C"
 
 # Import-Module posh-git
 # $GitPromptSettings.BranchColor.BackgroundColor = [ConsoleColor]::Magenta
 
 Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
-Invoke-Expression (&starship init powershell)
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -18,8 +18,8 @@ config.font = wezterm.font("FiraCode Nerd Font Mono")
 config.font_size = 12
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
-config.window_background_opacity = 0.5
-config.window_decorations = "NONE"
+config.window_background_opacity = 0.8
+config.window_decorations = "RESIZE"
 config.window_padding = { left = 0, right = 0, top = 5, bottom = 0 }
 
 config.leader = { key = "Tab", mods = "CTRL" }
