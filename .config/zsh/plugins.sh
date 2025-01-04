22f0d49de31c7f76af7d1b86ab0210652c15bb46
Dmitry Shirshov <shirshov.da@phystech.edu>
2025-01-04 16:13:12 +0300

    fix(zsh): use friendly names in antidote

--- .config/zsh/plugins.sh
+++ .config/zsh/plugins.sh
@@ -2,5 +2,6 @@ ANTIDOTE_PATH=$HOME/.local/share/antidote
 if ! [ -d "$ANTIDOTE_PATH" ]; then
 	git clone --depth=1 https://github.com/mattmc3/antidote.git "$ANTIDOTE_PATH"
 fi
+zstyle ':antidote:bundle' use-friendly-names 'yes'
 source "$ANTIDOTE_PATH/antidote.zsh"
 antidote load
