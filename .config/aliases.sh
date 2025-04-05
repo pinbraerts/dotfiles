e39822f1141983e3451a55d7c847db38f878b271
Dmitry Shirshov <shirshov.da@phystech.edu>
2025-04-05 15:03:41 +0300

    remove unused configurations

--- .config/aliases.sh
+++ .config/aliases.sh
@@ -12,7 +12,6 @@ alias_exists ccache sccache
 alias_exists xclip xclip -selection clipboard
 alias_exists m make
 alias_exists grep rg
-alias_exists gc git commit
 alias_exists gs git status
 alias_exists gl git log --all --decorate --oneline --graph
 alias_exists gcm git commit --message
@@ -21,14 +20,8 @@ alias_exists gp git push
 alias_exists gP git push --force
 alias_exists gf git fetch
 alias_exists gF git fetch --all --prune
-alias_exists gr git rebase
-alias_exists gri git rebase --interactive
-alias_exists gra git rebase --abort
-alias_exists grc git rebase --continue
-alias_exists go git checkout
 alias_exists g. git add .
 alias_exists ts tree-sitter
-alias_exists mm micromamba
 alias_exists sv systemctl
 alias_exists sudo doas
 alias mc="mc --nosubshell"
